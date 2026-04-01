a1188b3b6b3405a6371ad3be4aa1d1b6547b45aa
kyy1-cyy <iepjohn53@gmail.com>
2026-04-01 19:03:47 +1100

    Fix ReferenceError: express is not defined in uploads constructor

--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import { CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand, AbortMultipartUploadCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
